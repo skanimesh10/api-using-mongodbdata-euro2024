89ec3f8f214b2ed42b5da8d871230168e82c52f3
Animesh Swain <58727894+skanimesh10@users.noreply.github.com>
2024-08-01 20:57:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import logging
 from database import database
-from bson import ObjectId
 from pymongo import ASCENDING
 
 app = FastAPI()
@@ -9,35 +8,14 @@ logging.basicConfig(level=logging.INFO)
 
 
 def convert_objectid_to_str(doc):
-    """
-    Recursively converts ObjectId instances in a document to strings.
-
-    Args:
-        doc (Union[dict, list, ObjectId]): The document to convert.
-
-    Returns:
-        Union[dict, list, str]: The document with ObjectId instances converted to strings.
-    """
     if isinstance(doc, list):
         return [convert_objectid_to_str(item) for item in doc]
     if isinstance(doc, dict):
         return {key: convert_objectid_to_str(value) for key, value in doc.items() if key != "_id"}
-    return str(doc) if isinstance(doc, ObjectId) else doc
+    return str(doc) if hasattr(doc, 'binary') else doc
 
 
 async def fetch_and_convert(collection_name, query=None, sort=None, limit=1000):
-    """
-    Fetches documents from a MongoDB collection and converts ObjectId instances to strings.
-
-    Args:
-        collection_name (str): The name of the MongoDB collection.
-        query (dict, optional): The query to filter documents. Defaults to None.
-        sort (list of tuple, optional): The sort order for the documents. Defaults to None.
-        limit (int, optional): The maximum number of documents to fetch. Defaults to 1000.
-
-    Returns:
-        list: The list of documents with ObjectId instances converted to strings.
-    """
     query = query or {}
     cursor = database[collection_name].find(query)
     if sort:
@@ -53,40 +31,16 @@ def read_root():
 
 @app.get("/fixtures/")
 async def get_fixtures():
-    """
-    Endpoint to get a list of fixtures.
-
-    Returns:
-        list: The list of fixtures with ObjectId instances converted to strings.
-    """
     return await fetch_and_convert("euro-24-fixture", sort=[("fixture.id", ASCENDING)])
 
 
 @app.get("/standings/")
 async def get_standings():
-    """
-    Endpoint to get a list of standings.
-
-    Returns:
-        list: The list of standings with ObjectId instances converted to strings.
-    """
     return await fetch_and_convert("euro-24-standings")
 
 
 @app.get("/matches")
 async def get_match_by_id(id: int):
-    """
-    Endpoint to get a match by its fixture ID.
-
-    Args:
-        id (int): The fixture ID of the match.
-
-    Returns:
-        dict: The match document with ObjectId instances converted to strings.
-
-    Raises:
-        HTTPException: If the match with the given fixture ID is not found.
-    """
     logging.info(f"Searching for match with fixture.id: {id}")
     item = await database["euro-2024-matches"].find_one({"fixture.id": id})
     if item is None:
